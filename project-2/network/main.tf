31d67bdf83c597a4f91352eb02859349ca849e51
Moses <Moses.Owaseye@gmail.com>
2024-09-20 13:38:42 -0400

    added vnet-peering resource block

--- project-2/network/main.tf
+++ project-2/network/main.tf
@@ -17,3 +17,15 @@ resource "azurerm_virtual_network" "vnet" {
     intersite_network = var.lab_tag
   }
 }
+
+resource "azurerm_virtual_network_peering" "peering" {
+  for_each                  = var.vnet_address_space
+  name                      = format("peering-%s-to-%s", var.vnet_address_space.vnet-1.location, var.vnet_address_space.vnet-2.location)
+  resource_group_name       = azurerm_resource_group.rg[each.key].name
+  virtual_network_name      = azurerm_virtual_network.vnet[each.key].name
+  remote_virtual_network_id = azurerm_virtual_network.vnet[each.key].id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic   = true
+
+  allow_gateway_transit     = false  
+}
--- project-2/network/variables.tf
+++ project-2/network/variables.tf
@@ -1,21 +1,23 @@
+variable "subscription_id" {
+  type = string
+}
+
 variable "lab_tag" {
   default = "vnet peering with nva"
 }
 
 variable "vnet_address_space" { # chaining "for_each" between resources
   type = map(object({
-    name          = string
     address_space = string
     location      = string
   }))
   default = {
-    vnet-1 = { name          = "vnet1", 
-               address_space = "10.0.0.0/16",
+    vnet-1 = { address_space = "10.0.0.0/16",
                location      = "canadacentral" 
     },
-    vnet-2 = { name          = "vnet2", 
-               address_space = "172.16.0.0/16", 
+    vnet-2 = { address_space = "172.16.0.0/16", 
                location      = "canadaeast"
     }
   }
 }
+
