f4bafef19f468e0b4771847a787ef248755c06be
Moses <Moses.Owaseye@gmail.com>
2024-09-23 06:18:45 -0400

    added data.tf and 2nd spoke network

--- project-2/network/main.tf
+++ project-2/network/main.tf
@@ -1,31 +1,85 @@
-resource "azurerm_resource_group" "rg" {
-  for_each = var.vnet_address_space
-  name     = "rg-${each.value.location}"
-  location = each.value.location
+resource "azurerm_resource_group" "rg1" {
+  name     = var.rg1.name
+  location = var.rg1.location
   tags = {
-    intersite_network = var.lab_tag
+    intersite_network = "${var.lab_tag}-hub-spoke"
   }
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  for_each            = var.vnet_address_space
-  name                = "vnet-${each.value.location}"
-  address_space       = [each.value.address_space]
-  location            = azurerm_resource_group.rg[each.key].location
-  resource_group_name = azurerm_resource_group.rg[each.key].name
+resource "azurerm_resource_group" "rg2" {
+  name     = var.rg2.name
+  location = var.rg2.location
   tags = {
-    intersite_network = var.lab_tag
+    intersite_network = "${var.lab_tag}-spoke"
   }
 }
 
-resource "azurerm_virtual_network_peering" "peering" {
-  for_each                  = var.vnet_address_space
-  name                      = format("peering-%s-to-%s", var.vnet_address_space.vnet-1.location, var.vnet_address_space.vnet-2.location)
-  resource_group_name       = azurerm_resource_group.rg[each.key].name
-  virtual_network_name      = azurerm_virtual_network.vnet[each.key].name
-  remote_virtual_network_id = azurerm_virtual_network.vnet[each.key].id
+resource "azurerm_virtual_network" "vnet1" {
+  name                = var.vnet_peering-1.vnet_name
+  address_space       = [var.vnet_peering-1.address_space]
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  tags = {
+    intersite_network = "${var.lab_tag}-hub"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet2" {
+  name                = var.vnet_peering-2.vnet_name
+  address_space       = [var.vnet_peering-2.address_space]
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+  tags = {
+    intersite_network = "${var.lab_tag}-spoke1"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet3" {
+  name                = var.vnet_peering-3.vnet_name
+  address_space       = [var.vnet_peering-3.address_space]
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  tags = {
+    intersite_network = "${var.lab_tag}-spoke2"
+  }
+}
+
+resource "azurerm_virtual_network_peering" "peering1-2" {
+  name                      = format("peering-%s-to-%s", var.vnet_peering-1.vnet_name, var.vnet_peering-2.vnet_name)
+  resource_group_name       = azurerm_virtual_network.vnet1.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id = data.azurerm_virtual_network.vnet2.id
   allow_virtual_network_access = true
   allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
+}
 
-  allow_gateway_transit     = false  
+resource "azurerm_virtual_network_peering" "peering2-1" {
+  name                      = format("peering-%s-to-%s", var.vnet_peering-2.vnet_name, var.vnet_peering-1.vnet_name)
+  resource_group_name       = azurerm_virtual_network.vnet1.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.vnet2.name
+  remote_virtual_network_id = data.azurerm_virtual_network.vnet1.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
+}
+
+resource "azurerm_virtual_network_peering" "peering1-3" {
+  name                      = format("peering-%s-to-%s", var.vnet_peering-1.vnet_name, var.vnet_peering-3.vnet_name)
+  resource_group_name       = azurerm_virtual_network.vnet1.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id = data.azurerm_virtual_network.vnet3.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
+}
+
+resource "azurerm_virtual_network_peering" "peering3-1" {
+  name                      = format("peering-%s-to-%s", var.vnet_peering-3.vnet_name, var.vnet_peering-1.vnet_name)
+  resource_group_name       = azurerm_virtual_network.vnet3.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.vnet3.name
+  remote_virtual_network_id = data.azurerm_virtual_network.vnet1.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
 }
--- project-2/network/variables.tf
+++ project-2/network/variables.tf
@@ -2,22 +2,55 @@ variable "subscription_id" {
   type = string
 }
 
-variable "lab_tag" {
-  default = "vnet peering with nva"
+variable "rg1" {
+  type = object({
+    name = string 
+    location = string 
+  })
+  default = { name = "rg-canadacentral", location = "canadacentral" }
 }
 
-variable "vnet_address_space" { # chaining "for_each" between resources
-  type = map(object({
+variable "rg2" {
+  type = object({
+    name = string 
+    location = string 
+  })
+  default = { name = "rg-canadaeast", location = "canadaeast" }
+}
+
+variable "lab_tag" {
+  default = "vnet peering"
+}
+
+variable "vnet_peering-1" { 
+  type = object({
     address_space = string
-    location      = string
-  }))
+    vnet_name     = string
+  })
   default = {
-    vnet-1 = { address_space = "10.0.0.0/16",
-               location      = "canadacentral" 
-    },
-    vnet-2 = { address_space = "172.16.0.0/16", 
-               location      = "canadaeast"
-    }
+    address_space = "10.0.0.0/16"
+    vnet_name     = "vnet-1"
   }
 }
 
+variable "vnet_peering-2" { 
+  type = object({
+    address_space = string
+    vnet_name     = string
+  })
+  default = {
+    address_space = "172.16.0.0/16"
+    vnet_name     = "vnet-2"
+  }
+}
+
+variable "vnet_peering-3" { 
+  type = object({
+    address_space = string
+    vnet_name     = string
+  })
+  default = {
+    address_space = "192.168.0.0/16"
+    vnet_name     = "vnet-3"
+  }
+}
