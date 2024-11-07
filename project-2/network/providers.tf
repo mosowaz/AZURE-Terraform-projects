fa2cb12ee84d1f08545835d59666c6ee376611a8
Moses <150498910+mosowaz@users.noreply.github.com>
2024-11-06 20:52:50 -0500

    Update providers.tf

--- project-2/network/providers.tf
+++ project-2/network/providers.tf
@@ -16,9 +16,4 @@ provider "azurerm" {
     }
   }
   storage_use_azuread = true
-  
-  client_id       = data.azurerm_key_vault_secret.client_id.value
-  client_secret   = data.azurerm_key_vault_secret.client_secret.value
-  tenant_id       = data.azurerm_key_vault_secret.tenant_id.value
-  subscription_id = data.azurerm_key_vault_secret.subscription_id.value
 }
