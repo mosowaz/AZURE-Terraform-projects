cd13c0b92c062986dd1684280a8e96ff4970cac6
Moses <150498910+mosowaz@users.noreply.github.com>
2025-02-19 23:14:32 -0500

    create 2 instances of storage resources (#29)
    * modified nsg rules
    
    * create 2 instances of storage resources

--- project-3/infra/storage/main.tf
+++ project-3/infra/storage/main.tf
@@ -23,38 +23,38 @@ module "storage_account" {
   account_replication_type   = "LRS"
   account_tier               = "Standard"
   account_kind               = "StorageV2"
-  location                   = azurerm_resource_group.rg[each.key].location
-  name                       = "${each.value.prefix}${module.naming.storage_account[each.key].name_unique}"
+  location                   = azurerm_resource_group.rg.location
+  name                       = "${each.value.prefix}${module.naming.storage_account.name_unique}"
   https_traffic_only_enabled = true
-  resource_group_name        = azurerm_resource_group.rg[each.key].name
+  resource_group_name        = azurerm_resource_group.rg.name
   min_tls_version            = "TLS1_2"
   shared_access_key_enabled  = true
   # allow_nested_items_to_be_public = false
   public_network_access_enabled = true
   managed_identities = {
     system_assigned            = true
-    user_assigned_resource_ids = [azurerm_user_assigned_identity.vm[each.key].id]
+    user_assigned_resource_ids = [azurerm_user_assigned_identity.vm.id]
   }
   blob_properties = {
     versioning_enabled = true
   }
   role_assignments = {
     role_assignment_1 = {
-      role_definition_id_or_name       = data.azurerm_role_definition.role[each.key].name
-      principal_id                     = data.azurerm_client_config.current[each.key].object_id
+      role_definition_id_or_name       = data.azurerm_role_definition.role.name
+      principal_id                     = data.azurerm_client_config.current.object_id
       skip_service_principal_aad_check = false
     },
     role_assignment_2 = {
       role_definition_id_or_name       = "Owner"
-      principal_id                     = data.azurerm_client_config.current[each.key].object_id
+      principal_id                     = data.azurerm_client_config.current.object_id
       skip_service_principal_aad_check = false
     },
   }
   network_rules = {
     bypass                     = ["AzureServices"]
     default_action             = "Deny"
-    ip_rules                   = [data.azurerm_subnet.BastionSubnet[each.key].address_prefix]
-    virtual_network_subnet_ids = data.azurerm_subnet.BastionSubnet[each.key].id
+    ip_rule                    = data.azurerm_subnet.BastionSubnet.address_prefix
+    virtual_network_subnet_id  = data.azurerm_subnet.BastionSubnet.id
   }
   containers = {
     blob_container0 = {
