c042a1400e71c6b8c12165c646c4c90fe0080197
Moses <Moses.Owaseye@gmail.com>
2025-03-19 06:13:44 -0400

    edited vm-extension.yml and added jsonencode function

--- project-2/infra/vm-extention.tf
+++ project-2/infra/vm-extention.tf
@@ -9,20 +9,17 @@ resource "azurerm_virtual_machine_extension" "windows_custom_script" {
   type                 = "CustomScript"
   type_handler_version = "2.0"
 
-  settings = base64encode(<<SETTINGS
-   {
-    "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -Command \"
-      $storageAcct1Key = ${azurerm_storage_account.storage1.primary_access_key};
-      $acct1Key = ConvertTo-SecureString -String $storageAcct1Key -AsPlainText -Force;
-      $acct1credential = New-Object System.Management.Automation.PSCredential -ArgumentList ('Azure\\${azurerm_storage_account.storage1.name}'), $acct1Key;
-      New-PSDrive -Name Z -PSProvider FileSystem -Root '\\${azurerm_storage_account.storage1.name}.file.core.windows.net\\${azurerm_storage_share.share1.name}' -Credential $acct1credential;
+  settings = jsonencode(<<SETTINGS
+    commandToExecute: powershell.exe -ExecutionPolicy Unrestricted -Command
+    $storageAcct1Key = ${azurerm_storage_account.storage1.primary_access_key};
+    $acct1Key = ConvertTo-SecureString -String $storageAcct1Key -AsPlainText -Force;
+    $acct1credential = New-Object System.Management.Automation.PSCredential -ArgumentList ('Azure\${azurerm_storage_account.storage1.name}'), $acct1Key;
+    New-PSDrive -Name Z -PSProvider FileSystem -Root '\\${azurerm_storage_account.storage1.name}.file.core.windows.net\${azurerm_storage_share.share1.name}' -Credential $acct1credential;
 
-      $storageAcct2Key = ${azurerm_storage_account.storage2.primary_access_key};
-      $acct2Key = ConvertTo-SecureString -String $storageAcct2Key -AsPlainText -Force;
-      $acct2credential = New-Object System.Management.Automation.PSCredential -ArgumentList ('Azure\\${azurerm_storage_account.storage2.name}'), $acct2Key;
-      New-PSDrive -Name Y -PSProvider FileSystem -Root '\\${azurerm_storage_account.storage2.name}.file.core.windows.net\\${azurerm_storage_share.share2.name}' -Credential $acct2credential;
-    \""
-   }
+    $storageAcct2Key = ${azurerm_storage_account.storage2.primary_access_key};
+    $acct2Key = ConvertTo-SecureString -String $storageAcct2Key -AsPlainText -Force;
+    $acct2credential = New-Object System.Management.Automation.PSCredential -ArgumentList ('Azure\${azurerm_storage_account.storage2.name}'), $acct2Key;
+    New-PSDrive -Name Y -PSProvider FileSystem -Root '\\${azurerm_storage_account.storage2.name}.file.core.windows.net\${azurerm_storage_share.share2.name}' -Credential $acct2credential
     SETTINGS
   )
   protected_settings = <<PROTECTED_SETTINGS
@@ -43,18 +40,14 @@ resource "azurerm_virtual_machine_extension" "linux_custom_script" {
   type                 = "CustomScript"
   type_handler_version = "2.0"
 
-  settings = base64encode(<<SETTINGS
-    {
-    "commandToExecute": "bash -c \"
-          sudo mount -t cifs //${azurerm_storage_account.storage1.name}.file.core.windows.net/${azurerm_storage_share.share1.name} /mnt/azure-share1 \\
-          -o username=${azurerm_storage_account.storage1.name},password=${azurerm_storage_account.storage1.primary_access_key};
-          sudo mount -t cifs //${azurerm_storage_account.storage2.name}.file.core.windows.net/${azurerm_storage_share.share2.name} /mnt/azure-share2 \\
-          -o username=${azurerm_storage_account.storage2.name},password=${azurerm_storage_account.storage2.primary_access_key};
-        \""
-    }
+  settings = jsonencode(<<SETTINGS
+    commandToExecute: bash -c apt-get update && apt-get install cifs-utils && \
+    mount -t cifs ${azurerm_storage_account.storage1.name}.file.core.windows.net/${azurerm_storage_share.share1.name} /mnt/azure-share1 \
+    -o username=${azurerm_storage_account.storage1.name},password=${azurerm_storage_account.storage1.primary_access_key},dir_mode=0750,file_mode=0750.serverino && \
+    mount -t cifs ${azurerm_storage_account.storage2.name}.file.core.windows.net/${azurerm_storage_share.share2.name} /mnt/azure-share2 \
+    -o username=${azurerm_storage_account.storage2.name},password=${azurerm_storage_account.storage2.primary_access_key},dir_mode=0750,file_mode=0750,serverino 
     SETTINGS
   )
-
   protected_settings = <<PROTECTED_SETTINGS
     {
       "storageAccountName1": "${azurerm_storage_account.storage1.name}",
@@ -62,7 +55,7 @@ resource "azurerm_virtual_machine_extension" "linux_custom_script" {
       "storageAccountName2": "${azurerm_storage_account.storage2.name}",
       "storageAccountKey2": "${azurerm_storage_account.storage2.primary_access_key}"
     }
-  PROTECTED_SETTINGS
+    PROTECTED_SETTINGS
 } 
 
 
