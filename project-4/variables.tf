61b2b2da600195444cb1e7619819ca6056e2390a
Moses <Moses.Owaseye@gmail.com>
2025-03-29 07:11:25 -0400

    create a subnet in each vnet

--- project-4/variables.tf
+++ project-4/variables.tf
@@ -12,22 +12,30 @@ variable "vnet-int" {
   type = object({
     name = string
     address_space = string
+    subnet_name = string
+    subnet_address_prefixes = string
   })
   default = {
     name          = "int-lb-vnet"
     address_space = "10.1.0.0/16"
+    subnet_name = "int-lb-subnet"
+    subnet_address_prefixes = "10.1.0.0/24"
   }
-  description = "virtual network for internal loadbalancer backend pools"
+  description = "virtual network and subnet for internal loadbalancer backend pools"
 }
 
 variable "vnet-ext" {
   type = object({
     name = string
     address_space = string
+    subnet_name = string
+    subnet_address_prefixes = string
   })
   default = {
     name          = "ext-lb-vnet"
     address_space = "10.2.0.0/16"
+    subnet_name = "ext-lb-subnet"
+    subnet_address_prefixes = "10.2.0.0/24"
   }
-  description = "virtual network for external loadbalancer backend pools"
+  description = "virtual network and subnet for external loadbalancer backend pools"
 }
--- project-4/vnet-subnet.tf
+++ project-4/vnet-subnet.tf
@@ -1,17 +1,30 @@
-# Create vnet for internal backend pool 
+# Create vnet for internal LB backend pool 
 resource "azurerm_virtual_network" "vnet-int" {
   name                = var.vnet-int.name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = [var.vnet-int.address_space]
   dns_servers         = ["8.8.8.8", "9.9.9.9"]
+
+  subnet {
+    name             = var.vnet-int.subnet_name
+    address_prefixes = [var.vnet-int.subnet_address_prefixes]
+    # security_group   = azurerm_network_security_group.example.id
+  }
 }
 
-# Create vnet for external backend pool 
+# Create vnet for external LB backend pool 
 resource "azurerm_virtual_network" "vnet-ext" {
   name                = var.vnet-ext.name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = [var.vnet-ext.address_space]
   dns_servers         = ["8.8.8.8", "9.9.9.9"]
+
+  subnet {
+    name             = var.vnet-ext.subnet_name
+    address_prefixes = [var.vnet-ext.subnet_address_prefixes]
+    # security_group   = azurerm_network_security_group.example.id
+  }
 }
+
