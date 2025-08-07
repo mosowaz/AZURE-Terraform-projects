ef8dc69453262ec7287665522ebaf3ba9b2a20dd
Moses <Moses.Owaseye@gmail.com>
2025-08-07 11:09:38 -0400

    created subnets and pip

--- project-5/network.tf
+++ project-5/network.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${azurerm_resource_group.rg.name}-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.21.0.0/16"]
   dns_servers         = ["168.63.129.16", "9.9.9.9"]
 }
 
@@ -10,6 +10,29 @@ resource "azurerm_subnet" "backend" {
   name                 = "backend-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.21.1.0/24"]
 }
 
+resource "azurerm_subnet" "jumpbox" {
+  name                 = "jumpbox-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.21.2.0/27"]
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.21.3.0/24"]
+}
+
+# Create public IP for the frontend
+resource "azurerm_public_ip" "pip" {
+  allocation_method   = "Static"
+  name                = "${azurerm_resource_group.rg.name}-pip"
+  sku                 = "Standard"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  zones               = [1, 2, 3]
+}
